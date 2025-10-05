ecc050ab1a3598c5d068f6f2b926654c66ede16c
yadavupendraprasad <upendraprasad23.yadav@gmail.com>
2025-10-05 12:43:16 +0200

    add evaluiate file

---
+++ evaluate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+
+./env/bin/python src/models/evaluate_model.py
+
---
+++ run_train.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source env/bin/activate
+python src/models/search_model.py
